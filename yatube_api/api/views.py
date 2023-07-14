ae99cb96aa9bb263fbe6657f18d101226d1d9b1a
Григорий Серов <grisha200170@gmail.com>
2023-07-14 17:00:44 +0300

    second

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -11,7 +11,7 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
         # Read permissions are allowed to any request,
         # so we'll always allow GET, HEAD or OPTIONS requests.
         if request.method in permissions.SAFE_METHODS:
-            return True
+            return request.user.is_authenticated  # Add this line
 
         # Write permissions are only allowed to the author of the post.
         return obj.author == request.user
